42fabc0cabb2702ff04d06d1263aecd6a8f4334b
Mathieu <mathieukerbel@gmail.com>
2024-06-14 16:20:02 +0200

    multiple jobs fetcher - waiting for multithread implementation

--- src/result_fetcher.py
+++ src/result_fetcher.py
@@ -5,8 +5,15 @@ service = QiskitRuntimeService(
     instance='ibm-q/open/main',
     token='YOUR_API_TOKEN'
 )
-job = service.job('csp4wsvvwqp0008b49tg')
-job_result = job.result()
 
-for idx, pub_result in enumerate(job_result):
-    print(f"Expectation values for pub {idx}: {pub_result.data.evs}")
+current_ongoing_jobs = [
+    'csp4wsvvwqp0008b49tg',
+    'csp4wsk1k2e0008p7qdg',
+    'csp4wsb1k2e0008p7qd0'
+]
+
+for job_id in current_ongoing_jobs:
+    job = service.job(job_id)
+    job_result = job.result()
+    for idx, pub_result in enumerate(job_result):
+        print(f"Expectation values for pub {idx}: {pub_result.data.evs}")
